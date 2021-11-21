4772f3ad70e181aeb01cc3b2eb27b1f4f185f667
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2021-11-21 17:01:58 +0200

    Improved logic

--- optiprime.sh
+++ optiprime.sh
@@ -3,25 +3,46 @@
 APP_PATH="${1}"
 APP_ARGS="${@:2}"
 
-GPU_VENDOR="Intel"
+CHASSIS_TYPE="Desktop"
+GPU_TO_USE="Intel"
 
-if [ -d "/sys/class/power_supply/BAT0" ]; then
+# Check that bumblee is installed
+
+if [ ! -f "/usr/bin/bumblebeed" ]; then
+    echo "ERROR: bumblebee is not installed!"
+    exit 1
+fi
+
+# Detect battery state
+
+if [ -d "/sys/module/battery" ] \
+&& [ -d "/sys/class/power_supply/BAT0" ]
+&& [ -d "/proc/acpi/button/lid" ] ; then
     BATT_STATE=$(cat "/sys/class/power_supply/BAT0/status")
 
     echo "Battery state is '${BATT_STATE}'"
 
     if [ "${BATT_STATE}" = "Charging" ] || [ "${BATT_STATE}" = "Full" ] || [ "${BATT_STATE}" = "Unknown" ]; then
-        GPU_VENDOR="Nvidia"
+        GPU_TO_USE="Nvidia"
     fi
+else
+    echo "ERROR: No battery detected!"
+    echo "This script only works on laptops."
+
+    exit 2
 fi
 
-echo "Launching the application through the ${GPU_VENDOR} GPU..."
-echo "Application path: ${APP_PATH}"
-echo "Application args: ${APP_ARGS}"
+# Print info
 
-if [ "${GPU_VENDOR}" = "Intel" ]; then
+echo "Launching the application through the ${GPU_TO_USE} GPU..."
+echo " - Application path: ${APP_PATH}"
+echo " - Application args: ${APP_ARGS}"
+
+# Execute bin
+
+if [ "${GPU_TO_USE}" = "Intel" ]; then
     "${APP_PATH}" ${APP_ARGS}
-elif [ "$GPU_VENDOR" = "Nvidia" ]; then
+elif [ "$GPU_TO_USE" = "Nvidia" ]; then
     COMPRESSION_METHOD="jpeg" # yuv has similar results
 
     export vblank_mode=0
